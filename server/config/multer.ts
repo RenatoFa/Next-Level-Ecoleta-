4cf979c6d9acf6d89708281ee37dff327c0c8c47
Renato Ferreira de Assis <renato.v6@hotmail.com>
2020-07-01 22:18:51 -0300

    Configuração do multer

--- server/config/multer.ts
+++ server/config/multer.ts
@@ -1,5 +1,6 @@
 import multer from 'multer';
-import path from 'path'
+import path from 'path';
+import cryto from 'crypto'; // para um id diferente na imagens
 
 // Upload de Imagens
 
@@ -7,7 +8,10 @@ export default {
     storage: multer.diskStorage({
         destination: path.resolve(__dirname,'..','..','uploads'),
         filename: (request,file,callback)=>{
+        const hash = cryto.randomBytes(6).toString('hex');
+        const fileName = `${hash}-${file.originalname}`;
 
+        callback(null,fileName);
         }
     }),
 }
